a371b17093518595872fa5843d284fc8e4f77f12
dutta-sindhur <bunq.primary@gmail.com>
2026-01-22 22:48:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ const videoLoader = new VideoLoader();
 const videoAssets = [
   {
     id: 'showreel',
-    src: 'https://bunqlabs.github.io/bunq-labs-website-dec2025/assets/video/showreel_optimised.mp4',
+    src: 'https://bunqlabs.github.io/bunq-labs-website-dec2025/assets/video/showreel-v2.webm',
   },
 ];
 
